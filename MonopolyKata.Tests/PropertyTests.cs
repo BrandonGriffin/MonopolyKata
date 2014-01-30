800737caeb553c74d2282a50b9714e9b25455773
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-30 10:35:13 -0500

    Committing so I can pull to another machine.

--- MonopolyKata.Tests/PropertyTests.cs
+++ MonopolyKata.Tests/PropertyTests.cs
@@ -14,7 +14,7 @@ namespace MonopolyKata.Tests
         private Player player2;
         private List<Player> players;
         private Teller teller;
-        private Property property;
+        private Dictionary<Int32, Property> properties;
 
         [SetUp]
         public void SetUp()
@@ -23,14 +23,45 @@ namespace MonopolyKata.Tests
             player2 = new Player("Car");
             players = new List<Player> { player1, player2 };
             teller = new Teller(players);
-            property = new Property("Baltic Avenue", 60, 4, "Purple", teller);
+            properties = new Dictionary<Int32, Property> 
+            { 
+                { 1, new Property("Mediterranean Avenue", 60, 2,"Purple", teller) },
+                { 3, new Property("Baltic Avenue", 60, 4, "Purple", teller) },
+                { 5, new Property("Reading Railroad", 200, 25, "Railroad", teller) },
+                { 6, new Property("Oriental Avenue", 100, 6, "Light Blue", teller) },
+                { 8, new Property("Vermont Avenue", 100, 6, "Light Blue", teller) },
+                { 9, new Property("Connecticut Avenue", 120, 8, "Light Blue", teller) }, 
+                { 11, new Property("St. Charles Place", 140, 10, "Violet", teller) },
+                { 12, new Property("Electric Company", 150, 0, "Utility", teller) },
+                { 13, new Property("States Avenue", 140, 10, "Violet", teller) },
+                { 14, new Property("Virginia Avenue", 160, 12, "Violet", teller) },
+                { 15, new Property("Pennsylvania Railroad", 200, 25, "Railroad", teller) },
+                { 16, new Property("St. James Place", 180, 14, "Orange", teller) },
+                { 18, new Property("Tennessee Avenue", 180, 14, "Orange", teller) },
+                { 19, new Property("New York Avenue", 200, 16, "Orange", teller) },
+                { 21, new Property("Kentucky Avenue", 220, 18, "Red", teller) },
+                { 23, new Property("Indiana Avenue", 220, 18, "Red", teller) },
+                { 24, new Property("Illinois Avenue", 240, 20, "Red", teller) },
+                { 25, new Property("B & O Railroad", 200, 25, "Railroad", teller) },
+                { 26, new Property("Atlantic Avenue", 260, 22, "Yellow", teller) },
+                { 27, new Property("Ventor Avenue", 260, 22, "Yellow", teller) },
+                { 28, new Property("Water Works", 150, 0, "Utility", teller) },
+                { 29, new Property("Marvin Gardens", 280, 24, "Yellow", teller) },
+                { 31, new Property("Pacific Avenue", 300, 26, "Green", teller) },
+                { 32, new Property("North Carolina Avenue", 300, 26, "Green", teller) },
+                { 34, new Property("Pennsylvania Avenue", 320, 28, "Green", teller) },
+                { 35, new Property("Short Line", 200, 25, "Railroad", teller) },
+                { 37, new Property("Park Place", 350, 35, "Blue", teller) },
+                { 39, new Property("Boardwalk", 400, 50, "Blue", teller) },
+            };
+
         }
 
         [Test]
         public void LandingOnAnUnownedPropertyWillDeductThePurchaseAmountFromThePlayer()
         {
             var previousBalance = teller.bank[player1] = 200;
-            property.LandOnSpace(player1);
+            properties[3].LandOnSpace(player1);
 
             Assert.That(teller.bank[player1], Is.EqualTo(previousBalance - 60));
         }
@@ -40,8 +71,8 @@ namespace MonopolyKata.Tests
         {
             var previousBalance = teller.bank[player1] = 200;
 
-            property.LandOnSpace(player1);
-            var positionOwner = property.Owner;
+            properties[3].LandOnSpace(player1);
+            var positionOwner = properties[3].Owner;
 
             Assert.That(positionOwner, Is.EqualTo(player1));
         }
@@ -51,8 +82,8 @@ namespace MonopolyKata.Tests
         {
             var previousBalance = teller.bank[player1] = 200;
 
-            property.PassOverSpace(player1);
-            var positionOwner = property.Owner;
+            properties[3].PassOverSpace(player1);
+            var positionOwner = properties[3].Owner;
 
             Assert.That(positionOwner, Is.EqualTo(null));
         }
@@ -62,8 +93,8 @@ namespace MonopolyKata.Tests
         {
             var previousBalance = teller.bank[player1] = 200;
 
-            property.LandOnSpace(player1);
-            property.LandOnSpace(player1);
+            properties[3].LandOnSpace(player1);
+            properties[3].LandOnSpace(player1);
 
             var afterLandingOnMySpace = teller.bank[player1];
 
@@ -73,26 +104,40 @@ namespace MonopolyKata.Tests
         [Test]
         public void LandingOnAPropertyOwnedByAnotherPlayerDeductsRentFromMyAccount()
         {
-            property.Owner = player2;
+            properties[3].Owner = player2;
             var beforeLandingOnSpace = teller.bank[player1];
-            property.LandOnSpace(player1);
+            properties[3].LandOnSpace(player1);
 
             var afterLandingOnMySpace = teller.bank[player1];
 
-            Assert.That(afterLandingOnMySpace, Is.EqualTo(beforeLandingOnSpace - property.Rent));
+            Assert.That(afterLandingOnMySpace, Is.EqualTo(beforeLandingOnSpace - properties[3].Rent));
         }
 
         [Test]
         public void IfAnotherPlayerLandsOnMyPropertyMyAccountIsCreditedWithRent()
         {
-            property.Owner = player2;
+            properties[3].Owner = player2;
             var beforePropertyIsLandedOn = teller.bank[player2];
             
-            property.LandOnSpace(player1);
+            properties[3].LandOnSpace(player1);
 
             var afterPropertyIsLandedOn = teller.bank[player2];
 
-            Assert.That(afterPropertyIsLandedOn, Is.EqualTo(beforePropertyIsLandedOn + property.Rent));
+            Assert.That(afterPropertyIsLandedOn, Is.EqualTo(beforePropertyIsLandedOn + properties[3].Rent));
+        }
+
+        [Test]
+        public void IfAPlayerLandsOnARailroadOwnedByAnotherRailroadOwnerTheyPay50InRent()
+        {
+            properties[5].Owner = player2;
+            properties[15].Owner = player2;
+            var beforePropertyIsLandedOn = teller.bank[player1];
+
+            properties[15].LandOnSpace(player1);
+
+            var afterPropertyIsLandedOn = teller.bank[player1];
+
+            Assert.That(afterPropertyIsLandedOn, Is.EqualTo(beforePropertyIsLandedOn - 50));
         }
     }
 }
