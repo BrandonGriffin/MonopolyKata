ba02e0fbdf73ff0f46c4f5abc094e59a7e424a16
Brandon Griffin <bgriffin@qualtrax.com>
2014-02-12 13:26:13 -0500

    Eliminated some duplicate code by abstracting out card space.

--- MonopolyKata/Chance.cs
+++ MonopolyKata/Chance.cs
@@ -1,21 +1,5 @@
-﻿using System.Collections.Generic;
-
-namespace MonopolyKata
+﻿namespace MonopolyKata
 {
-    public class Chance : IBoardSpace
-    {
-        private Queue<ICard> cards;
-        
-        public void SetCards(Queue<ICard> cards)
-        {
-            this.cards = cards;
-        }
-
-        public void SpaceAction(Player player)
-        {
-            var card = cards.Dequeue();
-            card.Play(player);
-            cards.Enqueue(card);
-        }
-    }
+    public class Chance : CardSpace
+    { }
 }
--- MonopolyKata/CommunityChest.cs
+++ MonopolyKata/CommunityChest.cs
@@ -1,21 +1,5 @@
-﻿using System.Collections.Generic;
-
-namespace MonopolyKata
+﻿namespace MonopolyKata
 {
-    public class CommunityChest : IBoardSpace
-    {
-        private Queue<ICard> cards;
-        
-        public void SetCards(Queue<ICard> cards)
-        {
-            this.cards = cards;
-        }
-
-        public void SpaceAction(Player player)
-        {
-            var card = cards.Dequeue();
-            card.Play(player);
-            cards.Enqueue(card);
-        }
-    }
+    public class CommunityChest : CardSpace
+    { }
 }
