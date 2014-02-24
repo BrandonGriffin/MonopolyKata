4f1676ee044ce0d0771c5f7b524a48bbccececac
Brandon Griffin <bgriffin@qualtrax.com>
2014-02-24 09:22:57 -0500

    Changed the name of a lingering variable

--- MonopolyKata/Cards/AdvanceTo.cs
+++ MonopolyKata/Cards/AdvanceTo.cs
@@ -5,17 +5,17 @@ namespace MonopolyKata.Cards
     public class AdvanceTo : ICard
     {
         private Board board;
-        private Int32 goIndex;
+        private Int32 index;
 
-        public AdvanceTo(Board board, Int32 goIndex)
+        public AdvanceTo(Board board, Int32 index)
         {
             this.board = board;
-            this.goIndex = goIndex;
+            this.index = index;
         }
 
         public void Play(String player)
         {
-            board.MoveTo(player, goIndex);
+            board.MoveTo(player, index);
         }
     }
 }
